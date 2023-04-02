e0db7e4e8512bf24b7478bc13d540f814166f1a8
Shivangam Soni <shivangamsoni99@gmail.com>
2023-04-02 13:57:47 +0530

    Add Site Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
     return (
-        <div className="bg-blue-200 min-h-screen overflow-hidden">
-            <h1 className="text-violet-500 text-5xl">App</h1>
+        <div className="font-primary bg-neutral-white-100 text-neutral-black min-h-screen overflow-hidden">
+            <h1 className="text-5xl">App</h1>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "hsl(259, 100%, 65%)",
+                error: "hsl(0, 100%, 67%)",
+                neutral: {
+                    white: {
+                        50: "hsl(0, 0%, 100%)",
+                        100: "hsl(0, 0%, 94%)",
+                    },
+                    grey: {
+                        200: "hsl(0, 0%, 86%)",
+                        300: "hsl(0, 1%, 44%)",
+                    },
+                    black: "hsl(0, 0%, 8%)",
+                },
+            },
+            fontFamily: {
+                primary: "'Poppins', sans-serif;",
+            },
+        },
     },
     plugins: [],
 };
