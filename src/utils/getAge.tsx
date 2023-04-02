01e8c291cd4071f799f3a8da6e250b478bd49789
Shivangam Soni <shivangamsoni99@gmail.com>
2023-04-02 19:58:23 +0530

    Fix getAge Inaccuracy

--- src/utils/getAge.tsx
+++ src/utils/getAge.tsx
@@ -1,56 +1,13 @@
-// Reference: https://stackoverflow.com/a/54262003/16659219
 export function getAge(date: string) {
     const today = new Date();
     const birthday = new Date(date);
+    const diff = new Date(today.getTime() - birthday.getTime());
 
-    const age = {
-        years: today.getFullYear() - birthday.getFullYear(),
-        months: 0,
-        days: 0,
+    const epoch = new Date(0);
+
+    return {
+        years: Math.abs(diff.getUTCFullYear() - epoch.getUTCFullYear()),
+        months: Math.abs(diff.getUTCMonth() - epoch.getUTCMonth()),
+        days: Math.abs(diff.getUTCDate() - epoch.getUTCDate()),
     };
-
-    if (
-        birthday.getMonth() > today.getMonth() ||
-        (birthday.getMonth() === today.getMonth() &&
-            birthday.getDate() > today.getDate())
-    ) {
-        age.years -= 1;
-    }
-
-    let totalMonths =
-        (today.getFullYear() - birthday.getFullYear()) * 12 +
-        today.getMonth() -
-        birthday.getMonth();
-    totalMonths += today.getDay() < birthday.getDay() ? -1 : 0;
-
-    if (birthday.getDate() > today.getDate()) {
-        age.months = totalMonths % 12 || 11;
-        const currentMonth = today.getMonth();
-        const dateDiff = birthday.getDate() - today.getDate();
-
-        switch (currentMonth) {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12: {
-                age.days = 31 - dateDiff;
-                break;
-            }
-            default: {
-                age.days = 30 - dateDiff;
-                break;
-            }
-        }
-    } else {
-        age.days = today.getDate() - birthday.getDate();
-        age.months = totalMonths % 12;
-        if (birthday.getMonth() !== today.getMonth()) {
-            age.months += 1;
-        }
-    }
-
-    return age;
 }
