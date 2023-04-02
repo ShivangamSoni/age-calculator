cab30beef17e83615e86a2e53b2a77998acc372d
Shivangam Soni <shivangamsoni99@gmail.com>
2023-04-02 16:23:59 +0530

    Adjust Text Size

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -3,6 +3,8 @@ import { type ReactNode } from "react";
 export default function error({ children }: { children: ReactNode }) {
     if (!children) return null;
     return (
-        <span className="text-error text-xs sm:text-sm italic">{children}</span>
+        <span className="text-error text-xs sm:text-sm md:text-base italic">
+            {children}
+        </span>
     );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -25,7 +25,7 @@ export default forwardRef<HTMLInputElement, InputProps>(function input(
         <div className="flex flex-col justify-start gap-1">
             <label className="flex flex-col justify-start gap-1">
                 <span
-                    className={`text-xs sm:text-sm ${
+                    className={`text-xs sm:text-sm md:text-base ${
                         !error ? "text-neutral-grey-300" : "text-error"
                     } font-bold uppercase tracking-widest`}
                 >
@@ -34,7 +34,7 @@ export default forwardRef<HTMLInputElement, InputProps>(function input(
                 <input
                     ref={ref}
                     {...rest}
-                    className={`text-base sm:text-lg md:text-2xl font-bold tracking-wide p-3 border-none outline-none ring-1 ${
+                    className={`text-lg sm:text-2xl md:text-3xl font-bold tracking-wide p-3 border-none outline-none ring-1 ${
                         !error ? "ring-neutral-grey-200" : "ring-error"
                     } rounded-md transition-all duration-200 focus:ring-primary focus:ring-opacity-50 placeholder:font-bold placeholder:tracking-wide`}
                 />
