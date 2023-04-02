db24f6659e83ffc0e6dfc6df2a1cbc94978d6643
Shivangam Soni <shivangamsoni99@gmail.com>
2023-04-02 15:55:25 +0530

    Add Error Component

---
+++ src/components/Error.tsx
@@ -0,0 +1,8 @@
+import { type ReactNode } from "react";
+
+export default function error({ children }: { children: ReactNode }) {
+    if (!children) return null;
+    return (
+        <span className="text-error text-xs sm:text-sm italic">{children}</span>
+    );
+}
